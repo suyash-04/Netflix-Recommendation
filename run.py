3d0c72a2d9c67823b8ba14a6f26ba98c4ba7eafe
suyash <suyashadhikari04@gmail.com>
2025-05-16 23:04:07 +0545

    Change : port address

--- run.py
+++ run.py
@@ -34,4 +34,4 @@ if __name__ == '__main__':
     webbrowser.open_new(url)
     
     # Run the Flask application
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=80) 
